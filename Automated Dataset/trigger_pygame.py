9e7f101de49f8a53ddd251a0a19d1d8fdf984456
Syzygianinfern0 <spsharan2000@gmail.com>
2020-01-19 11:03:18 +0530

    Trigger code

---
+++ Automated Dataset/trigger_pygame.py
@@ -0,0 +1,27 @@
+import sys
+
+import pygame
+from pygame.locals import *
+
+
+def wait():
+    while True:
+        for event in pygame.event.get():
+            print(event)
+
+            if event.type == KEYDOWN:
+                pygame.quit()
+                sys.exit()
+
+
+def main():
+    pygame.init()
+    pygame.font.init()
+    display = pygame.display.set_mode((320, 240))
+    pygame.display.set_caption('Thermal Cam')
+    pygame.mouse.set_visible(True)
+    wait()
+
+
+if __name__ == '__main__':
+    main()
