391b21df849d788d91a65dfbabbf1e4e66634dcd
Syzygianinfern0 <spsharan2000@gmail.com>
2020-01-29 01:21:40 +0530

    Fix for Linux Systems Non Init for string vars

--- Automated Dataset/namer/files_gen.py
+++ Automated Dataset/namer/files_gen.py
@@ -1,10 +1,13 @@
 import glob,re,os
 counter=0
 data=""
+string1 = ""
+string2 = ""
+os.chdir("Dataset")
 for folder in glob.glob('*Cannon*'):
-        for filename in glob.glob(os.path.join(folder,"*small*long*.jpg")):
+        for filename in glob.glob(os.path.join(folder,"*small*long*.JPG")):
             string1 = filename
-        for filename in glob.glob(os.path.join(folder,"*small*short*.jpg")):
+        for filename in glob.glob(os.path.join(folder,"*small*short*.JPG")):
             string2 = filename
         data = string1 + "\t" + string2 + "\n" + data
         print(string1 + " " +string2)
@@ -13,9 +16,9 @@ with open("myFile_jpg.txt", "w") as myFile:
             myFile.write(data)
 data=""
 for folder in glob.glob('*Cannon*'):
-        for filename in glob.glob(os.path.join(folder,"*raw*long*.CR2")):
+        for filename in glob.glob(os.path.join(folder,"*raw*long*.CR3")):
             string1 = filename
-        for filename in glob.glob(os.path.join(folder,"*raw*short*.CR2")):
+        for filename in glob.glob(os.path.join(folder,"*raw*short*.CR3")):
             string2 = filename
         data = string1 + "\t" + string2 + "\n" + data
         print(string1 + " " +string2)
@@ -24,9 +27,9 @@ with open("myFile_raw.txt", "w") as myFile:
             myFile.write(data)
 data=""
 for folder in glob.glob('*Cannon*'):
-        for filename in glob.glob(os.path.join(folder,"*raw*long*.jpg")):
+        for filename in glob.glob(os.path.join(folder,"*raw*long*.JPG")):
             string1 = filename
-        for filename in glob.glob(os.path.join(folder,"*raw*short*.jpg")):
+        for filename in glob.glob(os.path.join(folder,"*raw*short*.JPG")):
             string2 = filename
         data = string1 + "\t" + string2 + "\n" + data
         print(string1 + " " +string2)
