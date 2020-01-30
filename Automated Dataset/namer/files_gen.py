174b7ef3de73ed67d791a633d88d73cd4c53eae7
Syzygianinfern0 <spsharan2000@gmail.com>
2020-01-30 16:07:37 +0530

    Crawl through thermal data

--- Automated Dataset/namer/files_gen.py
+++ Automated Dataset/namer/files_gen.py
@@ -1,38 +1,54 @@
-import glob,re,os
-counter=0
-data=""
+import glob
+import os
+
+counter = 0
+data = ""
 string1 = ""
 string2 = ""
-os.chdir("Dataset")
-for folder in glob.glob('*Cannon*'):
-        for filename in glob.glob(os.path.join(folder,"*small*long*.JPG")):
-            string1 = filename
-        for filename in glob.glob(os.path.join(folder,"*small*short*.JPG")):
-            string2 = filename
-        data = string1 + "\t" + string2 + "\n" + data
-        print(string1 + " " +string2)
-        counter+=1
+string3 = ""
+
+for folder in glob.glob('./Dataset/*Cannon*'):
+    for filename in glob.glob(os.path.join(folder, "*small*long*.JPG")):
+        string1 = filename
+    for filename in glob.glob(os.path.join(folder, "*small*short*.JPG")):
+        string2 = filename
+    for filename in glob.glob(os.path.join(folder, "temp.txt")):
+        string3 = filename
+    data = string1 + "\t" + string2 + "\t" + string3 + "\n" + data
+
+    print(string1 + " " + string2)
+    counter += 1
+
 with open("myFile_jpg.txt", "w") as myFile:
-            myFile.write(data)
-data=""
-for folder in glob.glob('*Cannon*'):
-        for filename in glob.glob(os.path.join(folder,"*raw*long*.CR3")):
-            string1 = filename
-        for filename in glob.glob(os.path.join(folder,"*raw*short*.CR3")):
-            string2 = filename
-        data = string1 + "\t" + string2 + "\n" + data
-        print(string1 + " " +string2)
-        counter+=1
+    myFile.write(data)
+
+data = ""
+for folder in glob.glob('./Dataset/*Cannon*'):
+    for filename in glob.glob(os.path.join(folder, "*raw*long*.CR3")):
+        string1 = filename
+    for filename in glob.glob(os.path.join(folder, "*raw*short*.CR3")):
+        string2 = filename
+    for filename in glob.glob(os.path.join(folder, "temp.txt")):
+        string3 = filename
+    data = string1 + "\t" + string2 + "\t" + string3 + "\n" + data
+    print(string1 + " " + string2)
+    counter += 1
+
 with open("myFile_raw.txt", "w") as myFile:
-            myFile.write(data)
-data=""
-for folder in glob.glob('*Cannon*'):
-        for filename in glob.glob(os.path.join(folder,"*raw*long*.JPG")):
-            string1 = filename
-        for filename in glob.glob(os.path.join(folder,"*raw*short*.JPG")):
-            string2 = filename
-        data = string1 + "\t" + string2 + "\n" + data
-        print(string1 + " " +string2)
-        counter+=1
+    myFile.write(data)
+
+data = ""
+for folder in glob.glob('./Dataset/*Cannon*'):
+    for filename in glob.glob(os.path.join(folder, "*raw*long*.JPG")):
+        string1 = filename
+    for filename in glob.glob(os.path.join(folder, "*raw*short*.JPG")):
+        string2 = filename
+    for filename in glob.glob(os.path.join(folder, "temp.txt")):
+        string3 = filename
+
+    data = string1 + "\t" + string2 + "\t" + string3 + "\n" + data
+    print(string1 + " " + string2)
+    counter += 1
+
 with open("myFile_raw_jpg.txt", "w") as myFile:
-            myFile.write(data)
+    myFile.write(data)
