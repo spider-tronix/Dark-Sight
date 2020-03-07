5e11f48470955a01db9f6fa9b81b3ee19008fc8a
Syzygianinfern0 <spsharan2000@gmail.com>
2020-03-07 10:24:20 +0530

    Training script complete

--- DarkNet/results/main.py
+++ DarkNet/results/main.py
@@ -1,5 +1,7 @@
 import gc
+import time
 
+import torch
 import torch.nn as nn
 from torch.optim import Adam
 from torch.utils.data import DataLoader
@@ -12,32 +14,35 @@ from models.vanilla_unet import VanillaUNet
 def main():
     net = VanillaUNet().cuda()
     trainset = Precious(simple_transform)
-    trainloader = DataLoader(trainset, batch_size=1, shuffle=True)
-    dataset = trainset.__getitem__(140)
+    trainloader = DataLoader(trainset, batch_size=4, shuffle=True)
 
-    # plt.imshow(np.hstack([dataset['short_img'].cpu().numpy().transpose((1, 2, 0)),
-    #                       dataset['long_img'].cpu().numpy().transpose((1, 2, 0)),
-    #                       ]))
-    # plt.show()
-
-    short = dataset['short_img'].unsqueeze(0)
-    temps = dataset['temps_img'].unsqueeze(0).unsqueeze(0)
-    long = dataset['long_img'].unsqueeze(0)
-
-    criterion = nn.L1Loss().cuda()
+    criterion = nn.L1Loss()
     optim = Adam(net.parameters(), 1e-4)
-    epochs = 1000
+    epochs = 150
 
     for epoch in range(epochs):
-        out = net(short, temps)
-        loss = criterion(out, long)
-        gc.collect()
-        optim.zero_grad()
-        loss.backward()
-        optim.step()
-        print(f"Epoch : {epoch} Loss : {loss.item()}")
-        del loss
-        del out
+        running_loss = 0
+
+        for i, data in enumerate(trainloader, 0):
+            short = data['short_img']
+            temps = data['temps_img'].unsqueeze(1)
+            long = data['long_img']
+
+            optim.zero_grad()
+
+            out = net(short, temps)
+            loss = criterion(out, long)
+            gc.collect()
+            loss.backward()
+            optim.step()
+            running_loss += loss
+            running_loss += loss.item()
+            if i % 18 == 17:
+                print('[%d, %5d] loss: %.3f' %
+                      (epoch + 1, i + 1, running_loss / 18))
+                running_loss = 0.0
+
+    torch.save(net.state_dict(), f'runs/{time.time()}.pth')
 
 
 if __name__ == '__main__':
