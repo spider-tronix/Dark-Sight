a6b47a657ec0a886ac1a42e01fb12df09747dfa4
Syzygianinfern0 <spsharan2000@gmail.com>
2020-03-07 16:29:40 +0530

    Some results updated

--- DarkNet/tests/time_model.py
+++ DarkNet/tests/time_model.py
@@ -4,12 +4,12 @@ setup = """
 import torch
 from models.vanilla_unet import VanillaUNet
 from results.configs import SIZE
-"""
-
-stmt = """
 net = VanillaUNet().cuda()
 low = torch.rand((1, 3, SIZE[0], SIZE[1])).cuda()
 temps = torch.rand((1, 1, 32, 24)).cuda()
+"""
+
+stmt = """
 net(low, temps)
 """
 
@@ -17,5 +17,5 @@ number = 100
 
 print(f'Avg time for Vanilla net : {timeit.timeit(stmt=stmt, setup=setup, number=number) / number} secs')
 
-# 1024 : 0.2311478357010001
-# 512 : 0.08542937483999595
+# 1024 : 0.1809366957499992
+# 512 : 0.04730567336000149
