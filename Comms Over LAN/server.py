af39718cf2a03b664fe389f1cc3ad871fc00560f
Syzygianinfern0 <spsharan2000@gmail.com>
2020-03-10 09:30:29 +0530

    Cleanup the comms folder

---
+++ Comms Over LAN/server.py
@@ -0,0 +1,23 @@
+import base64
+
+import cv2
+import zmq
+
+context = zmq.Context()
+footage_socket = context.socket(zmq.PUB)
+footage_socket.connect('tcp://192.168.43.156:5555')
+
+camera = cv2.VideoCapture(-1)  # init the camera
+
+while True:
+    try:
+        grabbed, frame = camera.read()  # grab the current frame
+        frame = cv2.resize(frame, (640, 480))  # resize the frame
+        encoded, buffer = cv2.imencode('.jpg', frame)
+        jpg_as_text = base64.b64encode(buffer)
+        footage_socket.send(jpg_as_text)
+
+    except KeyboardInterrupt:
+        camera.release()
+        cv2.destroyAllWindows()
+        break
