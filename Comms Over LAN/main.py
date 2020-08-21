2be4bf1f2a854daee73028f655d0a136b51a822e
sudharsan2000 <ssudharsan2000@gmail.com>
2020-08-21 20:11:55 +0530

    Made sensor read function.

--- Comms Over LAN/main.py
+++ Comms Over LAN/main.py
@@ -6,10 +6,12 @@ import socket
 import time
 
 from cv2 import cv2
+import collections
 import numpy as np
 from pssh.clients.native.parallel import ParallelSSHClient
 
-op = Array('d',24 * 32, lock=False)  #Global variable (shared memory between threads) 
+op = Array("d", 24 * 32, lock=False)  # Global variable (shared memory between threads)
+
 
 class Netcat:
     """ Python 'netcat like' module """
@@ -31,11 +33,11 @@ class Netcat:
     def read_until(self, data):
         """ Read data into the buffer until we have data """
         while data not in self.buff:
-            self.buff += self.conn.recv(1024).decode('ascii')
+            self.buff += self.conn.recv(1024).decode("ascii")
 
         pos = self.buff.find(data)
-        rval = self.buff[:pos + len(data)]
-        self.buff = self.buff[pos + len(data):]
+        rval = self.buff[: pos + len(data)]
+        self.buff = self.buff[pos + len(data) :]
 
         return rval
 
@@ -48,11 +50,13 @@ class Netcat:
 
 class ThermalCamera:
     def __init__(self):
-        self.port = '22'
-        self.uname = 'pi'
-        self.passd = 'ni6ga2rd'
-        self.ip = '192.168.0.109'
-        self.pi_ssh = ParallelSSHClient(hosts=[self.ip], user=self.uname, password=self.passd)
+        self.port = "22"
+        self.uname = "pi"
+        self.passd = "ni6ga2rd"
+        self.ip = "192.168.0.109"
+        self.pi_ssh = ParallelSSHClient(
+            hosts=[self.ip], user=self.uname, password=self.passd
+        )
         self.connect_ssh()
 
     def connect_ssh(self):
@@ -67,7 +71,9 @@ class ThermalCamera:
         # _, _, err = self.pi_ssh.exec_command('tmux send -t two "timeout 4 ~/bin/fbuf" ENTER', get_pty=True)
         # _ = self.pi_ssh.exec_command('tmux new-session -d "timeout 4 ~/bin/fbuf"')
 
-        self.pi_ssh.run_command(command='tmux new-session -d "~/test | nc 192.168.0.104 2000"')
+        self.pi_ssh.run_command(
+            command='tmux new-session -d "~/test | nc 192.168.0.104 2000"'
+        )
 
         # self.revive_cam()
         # err = err.readlines()
@@ -79,10 +85,10 @@ class ThermalCamera:
 
 
 def stdout2arr(string):
-    tem_row = list(string.strip().split('\n'))
+    tem_row = list(string.strip().split("\n"))
     temp = []
     for col in tem_row:
-        temp.append(list(map(float, col.strip().split(' '))))
+        temp.append(list(map(float, col.strip().split(" "))))
     temp = np.array(temp, dtype=np.float32)
 
     # temp = np.flipud(temp)
@@ -92,53 +98,71 @@ def stdout2arr(string):
 
 
 def arr2heatmap(arr):
-    heatmap = cv2.normalize(arr, dst=None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8U)
+    heatmap = cv2.normalize(
+        arr, dst=None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8U
+    )
     heatmap = cv2.applyColorMap(heatmap, cv2.COLORMAP_JET)
     return heatmap
 
+
 def thermal_process():
     global op
-    nc = Netcat('192.168.0.104', 2000)
+    nc = Netcat("192.168.0.104", 2000)
     cam = ThermalCamera()
     cam.trigger_camera()
 
     nc.listen()
-    _ = nc.read_until('End')
+    _ = nc.read_until("End")
 
     while True:
         try:
-            data = nc.read_until('End')
-            data = data[data.find('Subpage:') + 11:-4]
+            data = nc.read_until("End")
+            data = data[data.find("Subpage:") + 11 : -4]
             op[:] = list(np.concatenate(stdout2arr(data)))
             # print(op)
         except Exception as e:
             print(e)
 
 
-if __name__ == '__main__':
+def read_sensors(thermal_op_type="img", thermalimg_op_size=(24, 32)):
 
-    cap = picam.initialize()
+    Readings = collections.namedtuple("Readings", ["thermal", "normal"])
 
-    p = Process(target= thermal_process)
-    p.start()
+    thermal_readings = np.reshape(op[:], (-1, 32))
+    normal_img = picam.pi_img(cap)
 
+    vis = cv2.resize(thermal_readings, (thermalimg_op_size[0], thermalimg_op_size[1]))
+    heatmap = arr2heatmap(vis)
+
+    if thermal_op_type == "img":
+        reading = Readings(heatmap, normal_img)
+    else:
+        reading = Readings(thermal_readings, normal_img)
+
+    return reading
+
+
+def main():
 
     while True:
+        reading = read_sensors(thermalimg_op_size=(480, 360))
 
-        tick = time.time()
-        thermal_readings = np.reshape(op[:],(-1,32))
-        normal_img = picam.pi_img(cap)
+        cv2.imshow("thermal", reading.thermal)
+        cv2.imshow("image", reading.normal)
 
-        vis = cv2.resize(thermal_readings, (480, 360))
-        heatmap = arr2heatmap(vis)
-        tock = time.time()
-        print('FPS: ', 1/(tock-tick))
-
-        cv2.imshow('thermal', heatmap)
-        cv2.imshow('image', normal_img)
-
-        if cv2.waitKey(1)&0XFF == ord('q'):
+        if cv2.waitKey(1) & 0xFF == ord("q"):
             break
 
     cap.release()
     cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+
+    cap = picam.initialize()
+
+    p = Process(target=thermal_process)
+    p.start()
+
+    main()
+
--- Comms Over LAN/picam_client.py
+++ Comms Over LAN/picam_client.py
@@ -5,15 +5,16 @@ def initialize():
 
     print('Connecting to PI cam...')
     cap = cv2.VideoCapture('udp://192.168.0.104:1234',cv2.CAP_FFMPEG)
+    
     while not cap.isOpened():
-        
         cv2.waitKey(10)
+
     print('Pi cam connected!')
     return cap
 
 
 def pi_img(cap):
-
+    
     ret, frame = cap.read()
 
     # cv2.imshow('image here', frame)
